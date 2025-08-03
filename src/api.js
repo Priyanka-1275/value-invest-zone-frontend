952b0f84465dd697f7a06262e6ed0abeb0ec6b3e
Priyanka-1275 <priyankakothapalli127@gmail.com>
2025-08-03 22:42:41 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // Replace with your backend if deployed
+const API_BASE_URL = ' https://value-invest-zone-20nq.onrender.com'; // Replace with your backend if deployed
 
 export const uploadProperty = async (propertyData) => {
   const response = await axios.post(`${API_BASE_URL}/api/properties`, propertyData);
@@ -11,3 +11,4 @@ export const fetchAllProperties = async () => {
   const response = await axios.get(`${API_BASE_URL}/api/properties`);
   return response.data;
 };
+
